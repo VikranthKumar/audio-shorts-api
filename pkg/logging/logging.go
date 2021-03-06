1fe10de8082cf25af2edc8c2739edb50958ff908
Irham <m.irham.agung@gmail.com>
2021-03-06 12:03:17 +0800

    refined logs to write to file

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -5,26 +5,31 @@ import (
 	"go.uber.org/zap"
 )
 
-const LOGGER_KEY = 1
+const (
+	LoggerKey = 1
+)
 
 var logger *zap.Logger
 
 func initLogger() {
-	logger, _ = zap.NewDevelopment()
+	cfg := zap.NewDevelopmentConfig()
+	cfg.DisableStacktrace = true
+	cfg.OutputPaths = []string{"logfile.log", "stdout"}
+	logger, _ = cfg.Build()
 }
 
 func NewContext(ctx context.Context, fields ...zap.Field) context.Context {
 	if logger == nil {
 		initLogger()
 	}
-	return context.WithValue(ctx, LOGGER_KEY, WithContext(ctx).With(fields...))
+	return context.WithValue(ctx, LoggerKey, WithContext(ctx).With(fields...))
 }
 
 func WithContext(ctx context.Context) *zap.Logger {
 	if ctx == nil {
 		return logger
 	}
-	if ctxLogger, ok := ctx.Value(LOGGER_KEY).(*zap.Logger); ok {
+	if ctxLogger, ok := ctx.Value(LoggerKey).(*zap.Logger); ok {
 		return ctxLogger
 	}
 	return logger
